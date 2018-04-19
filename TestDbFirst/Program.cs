00ac491089eec2f3f847d60e251163b03b34b9e7
Trainer <trainer@stepit.loc>
2018-04-19 20:57:16 +0300

    Add first version

---
+++ TestDbFirst/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDbFirst
+{
+	class Program
+	{
+		static void AddPackage(Package package)
+		{
+			using (LibraryEntities db = new LibraryEntities())
+			{
+				db.Package.Add(package);
+				db.SaveChanges();
+				Console.WriteLine(package.Name+ " ");
+			}
+		}
+
+
+		static void Main(string[] args)
+		{
+
+			Package package = new Package
+			{
+				Name = "X"
+			};
+			AddPackage(package);
+			
+		}
+	}
+}
